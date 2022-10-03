367c0bed3977e79178d28ea3f95ecd8460be79a0
Truong Vuong <truong.vuong@ekoios.vn>
2022-10-04 01:18:12 +0700

    update amend

--- switchcase.js
+++ switchcase.js
@@ -1,13 +1,13 @@
-// const option = 'all';
-// const filterDictionary = {
-//     all: 'All items',
-//     category1: 'Category1 items',
-//     category2: 'Category2 items',
-//     category3: 'Category3 items',
-//     category4: 'Category4 items'
-// }
+const option = 'all';
+const filterDictionary = {
+    all: 'All items',
+    category1: 'Category1 items',
+    category2: 'Category2 items',
+    category3: 'Category3 items',
+    category4: 'Category4 items'
+}
 
-// console.log(filterDictionary['all']);
+console.log(filterDictionary['all']);
 
 const state = {
     a: 3,
